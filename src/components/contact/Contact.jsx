65a003738046ba4bdc9819fcf848a7b258812454
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-02 16:28:41 +0500

    Building contact component

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./contact.css";
+
+const Contact = () => {
+	return (
+		<div className="c">
+			<div className="c-bg"></div>
+		</div>
+	);
+};
+
+export default Contact;
