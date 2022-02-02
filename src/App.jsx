0b42ba4d3e2c79dd00a1f049a3223260f09b947b
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-02 12:50:04 +0500

    About Component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from "./components/about/About";
 import Intro from "./components/intro/intro";
 
 const App = () => {
 	return (
 		<div>
 			<Intro />
+			<About />
 		</div>
 	);
 };
