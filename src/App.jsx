1e42a92d4ff7e187cb39a3601468b7918b9275a2
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-02 16:25:29 +0500

    Product component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./components/about/About";
 import Intro from "./components/intro/intro";
+import ProductList from "./components/productList/ProductList";
 
 const App = () => {
 	return (
 		<div>
 			<Intro />
 			<About />
+			<ProductList />
 		</div>
 	);
 };
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+	{
+		id: 1,
+		img: "https://images.pexels.com/photos/9420580/pexels-photo-9420580.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+		link: "http://lama.dev",
+	},
+	{
+		id: 2,
+		img: "https://images.pexels.com/photos/10530054/pexels-photo-10530054.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+		link: "http://lama.dev",
+	},
+	{
+		id: 3,
+		img: "https://images.pexels.com/photos/11028655/pexels-photo-11028655.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		link: "http://lama.dev",
+	},
+	{
+		id: 4,
+		img: "https://images.pexels.com/photos/5873299/pexels-photo-5873299.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		link: "http://lama.dev",
+	},
+	{
+		id: 5,
+		img: "https://images.pexels.com/photos/10659976/pexels-photo-10659976.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		link: "http://lama.dev",
+	},
+	{
+		id: 6,
+		img: "https://images.pexels.com/photos/7855300/pexels-photo-7855300.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		link: "http://lama.dev",
+	},
+];
