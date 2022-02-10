d027371b51de88dee94b292d309bebc44e7e6df8
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-10 16:41:36 +0500

    Updated

--- src/data.js
+++ src/data.js
@@ -1,9 +1,8 @@
 export const products = [
 	{
 		id: 1,
-		// img: "https://previews.dropbox.com/p/thumb/ABano-g5ZuhP042H7wO9ayTKOsn8PPMtM0pUFzVUEdlMN_lWx6sm3XAnZVbinM4GXSV_vvGCkZ5_kfTMu1KwD5D2Yop0cO3jYD81ZD8Z4FflgKF9fEGkzLFG7_hsabVlsdDcW6YpcU2fWeo9gDAQV46RcaKYd6aLKdKL-fZkoZN2SQSH5jMeu1K8dZKx0OyOrxrM0bRwN2TC8eddyBYZIDFu0GKhJuMuzYtmLXcI_seiwm9teNOe9h6byxPc0djPyvzC7otGx-ZDI5k-eO0XxAyyMxNrx7MVUb4wMKaFlnrPgn7xgjE9mZQ5J5s4kWVarBjS1A395lScQf3PKjR4ladb5t_vbIvzO47PsoygW306JQ/p.png",
-		img: "https://dc613.4shared.com/img/s0Y7LnwJiq/s23/17ec0912dd8/colors-generator-react-app",
-		link: "https://colors-generator-reactapp.netlify.app/",
+		img: "https://dc619.4shared.com/img/5oRxjYpSea/s24/17ee36f4fb0/instagram-clone-reactapp?async&rand=0.6668398830479385",
+		link: "https://instagram-clone-reactapp-db0d8.firebaseapp.com/",
 	},
 	{
 		id: 2,
@@ -27,7 +26,7 @@ export const products = [
 	},
 	{
 		id: 6,
-		img: "https://images.pexels.com/photos/7855300/pexels-photo-7855300.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-		link: "http://lama.dev",
+		img: "https://dc613.4shared.com/img/s0Y7LnwJiq/s23/17ec0912dd8/colors-generator-react-app",
+		link: "https://colors-generator-reactapp.netlify.app/",
 	},
 ];
